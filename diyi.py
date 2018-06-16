2cc3f9fb0c242b652cae119c0566e577c14ddbc6
313627345@qq.com <313627345@qq.com>
2018-06-16 17:56:23 +0800

    20180616

---
+++ diyi.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+#定义函数
+def my_abs(x):
+    if x >= 0:
+        return x
+    else:
+        return -x
+
+#空函数
+# 你这个语句没有想好怎么写，但是又要让其运行起来 可以使用 pass 让代码先运行起来！
+if age >= 18:
+    pass
+#返回多个值
+def luck(x,y):
+    nx=x*x
+    ny=y+y
+    return nx,ny
+
+#over
